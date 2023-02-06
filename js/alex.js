f2461ccf8e728df7227b37f21b56299015101348
Saurabh Mishra <saurabh@simformsolutions.com>
2023-02-06 14:26:21 +0530

    update image slider jQuery to javaScript

--- js/alex.js
+++ js/alex.js
@@ -1,31 +1,32 @@
-$(document).ready(function(){
-    $('#logo').click(function(){
-       $('#mainIntro').css('background-color','#E3E3E3').slideUp(2000).slideDown(2000);
-    });
-
-    $(".divs div").each(function(e) {
-        if (e != 0)
-            $(this).hide();
-    });
-
-    $("#next").click(function(){
-        if ($(".divs div:visible").next().length != 0)
-            $(".divs div:visible").next().show().prev().hide();
-        else {
-            $(".divs div:visible").hide();
-            $(".divs div:first").show();
-        }
-        return false;
-    });
-
-    $("#prev").click(function(){
-        if ($(".divs div:visible").prev().length != 0)
-            $(".divs div:visible").prev().show().next().hide();
-        else {
-            $(".divs div:visible").hide();
-            $(".divs div:last").show();
-        }
-        return false;
+$(document).ready(function () {
+    $('#logo').click(function () {
+        $('#mainIntro').css('background-color', '#E3E3E3').slideUp(2000).slideDown(2000);
     });
 });
+//Image slider functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(num) {
+    showSlides(slideIndex += num);
+}
+
+function currentSlide(num) {
+    showSlides(slideIndex = num);
+}
+
+function showSlides(num) {
+    let i;
+    let slides = document.getElementsByClassName("myImages");
+    if (num > slides.length){
+        slideIndex = 1
+    }
+    if (num < 1){ 
+        slideIndex = slides.length 
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
 
